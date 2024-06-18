b893591d25ea77b47d082c82454092edb85c8369
jagadeeshwar <mummadijagadeesh143@gmail.com>
2024-06-18 23:03:10 +0530

    docker compose file created

--- roboshop/docker.sh
+++ roboshop/docker.sh
@@ -5,3 +5,13 @@ cd ../catalogue
 docker build -t catalogue:1.0.0 .
 cd ../web
 docker build -t web:1.0.0 .
+cd ../user
+docker build -t user:1.0.0 .
+cd ../cart
+docker build -t cart:1.0.0 .
+cd ../mysql
+docker build -t mysql:1.0.0 .
+cd ../shipping
+docker build -t shipping:1.0.0 .
+cd ../payment
+docker build -t payment:1.0.0 .
